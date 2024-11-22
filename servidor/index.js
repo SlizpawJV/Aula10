facf9387b412bb4c20d4849dc53d238640cbb4a3
Sliz <etecaluno6@gmail.com>
2024-11-22 12:21:52 -0300

    receba

---
+++ servidor/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2');
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'aulabd'
+});
+
+db.connect((erro) => {
+    if (erro) {
+        console.error('erro ao conectar ao MySQL: ', erro);
+    } else {
+        console.log('conectado ao mysql com sucesso');
+    }
+});
+
+app.post('/alunos', (req, res) => {
+    const {nome, cidade, estado} = req.body;
+
+    const sql = 'INSERT INTO alunos (nome,cidade,estado) VALUES (?,?,?)';
+
+    db.query(sql, [nome,cidade,estado], (err, result) =>{
+        if (err)
+        {
+            return res.status(500).json({error: 'erro ao cadastrar aluno'});
+
+        }
+        res.status(201).json({message: 'aluno cadastrado com sucesso', id: result.insertId});
+
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`servidor rodando em http://localhost:${PORT}`);
+});
